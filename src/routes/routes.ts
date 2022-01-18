c2ad95e8191df625a9693e8004b98678bafb886b
yud-cumba <yudith.cumba@gmail.com>
2022-01-18 12:23:28 -0500

    feat: add nested routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { lazy, LazyExoticComponent } from 'react';
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { NoLazy } from '../01-lazyload/pages/NoLazy';
 type JSXElement = () => JSX.Element;
 interface Route {
     to: string;
@@ -9,27 +10,40 @@ interface Route {
     Component: LazyExoticComponent<JSXElement> | JSXElement;
 }
 
-const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../01-lazyload/pages/LazyPage3'));
+// const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../01-lazyload/pages/LazyPage1'));
+// const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../01-lazyload/pages/LazyPage2'));
+// const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../01-lazyload/pages/LazyPage3'));
+const LazyLayout = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../01-lazyload/layout/LazyLayout'));
 
 export const routes: Route[] = [
+    // {
+    //     to: '/lazy1',
+    //     path: 'lazy1',
+    //     Component: Lazy1,
+    //     name: 'LazyPage1'
+    // },
+    // {
+    //     to: '/lazy2',
+    //     path: 'lazy2',
+    //     Component: Lazy2,
+    //     name: 'LazyPage2'
+    // },
+    // {
+    //     to: '/lazy3',
+    //     path: 'lazy3',
+    //     Component: Lazy3,
+    //     name: 'LazyPage3'
+    // },
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: Lazy1,
-        name: 'LazyPage1'
+        to: '/lazyload/',
+        path: '/lazyload/*',
+        Component: LazyLayout,
+        name: 'LazyLayout - Dashboard'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: Lazy2,
-        name: 'LazyPage2'
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: Lazy3,
-        name: 'LazyPage3'
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'Lazy-2'
     },
 ]
